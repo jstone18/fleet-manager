9804ba3b0e52290ff91de8aaab7eb7368df5e802
jstone18 <jason.stone0808@gmail.com>
2019-11-17 21:47:55 -0500

    add json-server and materialize support

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
 
-function App() {
-	return (
-		<div className="App">
-			<h1>Fleet Manager</h1>
-		</div>
-	);
-}
+const App = () => {
+	useEffect(() => {
+		// Init Materialize JS
+		M.AutoInit();
+	});
+	return <div className="App">Fleet Manager</div>;
+};
 
 export default App;
